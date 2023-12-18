e142363870e77244ae2131fde6770ca9161cfa66
almight3 <shrivastavap136@gmail.com>
2023-12-19 00:14:12 +0530

    pull from main

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -1,16 +1,20 @@
 import {useState} from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoIosSearch } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
 
 export default Headers = ()=>{
     return(
-        <div className="w-full drop-shadow">
-        <RxHamburgerMenu />
-        <div>
-            <input />
-            <IoIosSearch />
+        <div className="w-full  grid grid-cols-3 p-4 shadow-md">
+        <RxHamburgerMenu size={30} className="cursor-pointer" />
+        <div className="flex">
+            <input className="border border-slate-300 w-3/4 p-1 focus:outline-none	" />
+             <button className="bg-slate-200 px-5 rounded-r-lg">
+             <IoIosSearch  size={25} />
+             </button>
         </div>
+        <CgProfile size={30}  className="place-self-end	"/>
        </div>
     )
    
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default Sidebar = ()=>{
+    return(
+      <div className="w-80">
+         <div>
+            <ul>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li></li>
+                <li></li>
+            </ul>
+         </div>
+      </div> 
+    )
+}
